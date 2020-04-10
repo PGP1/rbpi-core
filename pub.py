6c450238331787812c416772295838bbaecc0bf1
Duy Nguyen <41281749+nugyn@users.noreply.github.com>
2020-04-10 20:28:42 +1000

    4 rpimqtt (#7)
    * added subscribe.py script
    
    * modfied pub.py to publish to localgate_to_awsiot
    
    * clean up repo - removed .swp file
    
    * refactoring of code, added README for PI set up
    
    * [US3.11] removed double looping, no longer causes error
    
    * refactoring pub.py and sub.py
    
    Co-authored-by: Debian <admin@ip-172-31-0-243.ap-southeast-2.compute.internal>
    Co-authored-by: pi-default <pi@pgp1.com>

---
+++ pub.py
@@ -0,0 +1,46 @@
+import paho.mqtt.client as mqtt
+import serial
+
+#arduino setup
+ser = serial.Serial('/dev/ttyACM0',9600)
+s = [0]
+
+'''
+main methods
+- connect()
+- disconnect()
+- subscribe()
+- publish ()
+'''
+
+broker_address = "110.174.81.168"
+port = 1883
+topic = "localgateway_to_awsiot"
+
+def on_publish(client, userdata, result):
+    print("data published \n")
+    pass
+
+def on_disconnect(client, userdata, rc):
+    logging.debug("disconnected, rc=",str(rc))
+    client.loop_stop()
+    print("client disconnected OK")
+        
+# create new instance
+client = mqtt.Client("awsiot-client")
+client.on_publish = on_publish
+client.on_disconnect = on_disconnect
+
+# client.publish("sensor/water_level","LOW")
+
+# set broker address of raspberry pis
+# connect to pi
+client.connect(broker_address,port)
+# publish a message
+
+read_serial=ser.readline()
+s[0] = str(int (ser.readline(),16))
+
+#Publish to topic 'localgateway_to_awsiot' for AWS IoT to pickup
+client.publish(topic, s[0])
+client.disconnect()
---
+++ sub.py
@@ -0,0 +1,33 @@
+import paho.mqtt.client as mqtt
+import time as time
+
+user = "user"
+password = "password"
+topic = "awsiot_to_localgateway"
+broker_address="110.174.81.168"
+port = "1883"
+
+#initialise MQTT Client
+client = mqtt.Client("pi-subscriber")
+
+def on_connect(client, userdata, flags, rc):
+    if rc==0:
+        print("connection established, returned code=",rc)
+        client.subscribe(topic)
+    else:
+        print("connection error, returned code=",rc)
+
+def on_message(client, userdata, msg):
+    print("topic: {} | payload: {} ".format(msg.topic, msg.payload))
+
+def on_log(client, userdata, level, buf):
+    print("log ", buf)
+
+# Binds functions defined above, on connection, message and log
+client.on_connect = on_connect
+client.on_message = on_message
+client.on_log = on_log
+
+#client.username_pw_set(user, password)
+client.connect(broker_address)
+client.loop_forever()
