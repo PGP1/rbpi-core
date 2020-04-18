aad3b386110bfb375c14dc2d898c74c4ef25fdc5
Duy Nguyen <41281749+nugyn@users.noreply.github.com>
2020-04-19 07:49:58 +1000

    Removed test code

--- arduinoconnect.py
+++ arduinoconnect.py
@@ -1,13 +1,10 @@
 from time import sleep
-# import serial
+import serial
 from pub import Publisher
 
 #Established arduino connection
-ser = ""
-s = {"message": "test"}
-
-# ser = serial.Serial('/dev/ttyACM0', 9600)
-# s = [0]
+ser = serial.Serial('/dev/ttyACM0', 9600)
+s = [0]
 
 def push_data():
         publisher = Publisher()
