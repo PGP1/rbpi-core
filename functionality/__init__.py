87ead16f3a7d8751ebfea83d367e71c6a11dbca2
nugyn <s3429599@student.rmit.edu.au>
2020-04-19 20:00:16 +1000

    fixed error in json.load (loadconfig.json)

--- functionality/__init__.py
+++ functionality/__init__.py
@@ -1,3 +1,2 @@
 # __init__.py
-from .arduinoconnect import push_data
-from .unlinkdevice import *
+
--- utility/loadconfig.py
+++ utility/loadconfig.py
@@ -3,7 +3,6 @@ import json
 
 def load_config():
     with open('./config/config.json', encoding='utf-8-sig') as configuration:
-        config_file = configuration.read()
-        config_json = json.load(config_file)
+        config_json = json.load(configuration.read())
 
     return config_json
