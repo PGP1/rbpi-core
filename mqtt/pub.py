9bf72f1ace812a9de414a3066f430d02cfb1eda8
Duy Nguyen <41281749+nugyn@users.noreply.github.com>
2020-04-29 10:40:04 +1000

    [US3.3.2] Stream Data to s3: JSON Object Push (#13)
    * Arduino Serial data connection, and added timestamp isocalendar format
    * Refactoring JSON construction for publish payload

--- mqtt/pub.py
+++ mqtt/pub.py
@@ -3,6 +3,7 @@ import json
 import logging
 import os
 import utility
+import datetime
 from dotenv import load_dotenv
 env_path = './.env'
 load_dotenv(dotenv_path=env_path)
@@ -20,10 +21,6 @@ methods
 
 
 class Publisher:
-    pub = ""
-    broker_address = ""
-    port = ""
-
     def __init__(self):
         self.broker_address = str(BROKER_IP)
         self.port = int(BROKER_PORT)
@@ -38,25 +35,34 @@ class Publisher:
         print("client disconnected OK")
 
     def publish(self, pub, arduinopayload):
-        print(self.broker_address)
-        print(self.port)
+        print("Sending to: ", self.broker_address)
+        print("on: ", self.port)
         if pub == 'arduino':
             # setting topic to publish to
             topic = utility.loadconfig.load_config()['topic']['toawsiot/b1']
-            id = utility.iddevice.get_id()
-            payload = {'broker-device': {'pi-id' : id}, 'message': arduinopayload}
+            brokerID= utility.iddevice.get_id()
+            now_time = datetime.datetime.now().isoformat()
+            
+            publishJSON = {}
+            payload = {}
+            
+            publishJSON['broker-device'] = brokerID
+            payload['time'] = now_time
+            data = arduinopayload
+            payload['data'] = data
+            publishJSON['payload'] = payload
 
             # create new instance
             client = mqtt.Client("awsiot-client")
             client.on_publish = self.on_publish
             client.on_disconnect = self.on_disconnect
-
+            
             # set broker address of raspberry pis
             # connect to pi
             client.connect(self.broker_address, self.port)
-
-            # Publish to topic 'localgateway_to_awsiot/b1' for AWS IoT to pickup
-            client.publish(topic, json.dumps(payload))
+            
+            #Publish to topic 'localgateway_to_awsiot/b1' for AWS IoT to pickup
+            client.publish(topic, json.dumps(publishJSON))
             client.disconnect()
         elif pub == 'status':
             # Publish back to the AWSIoT to respond for request for online
@@ -69,6 +75,6 @@ class Publisher:
 
             topic = loadconfig.load_config()['topic']['toawsiot/b1']
             id = iddevice.get_id()
-            payload = {'device': {'pi-id' : id}, 'message': 'on'}
-            client.publish(topic, json.dumps(json.dumps(payload)))
+            payload = {'broker-device': id, 'payload': 'On'}
+            client.publish(topic, json.dumps(payload))
             client.disconnect()
