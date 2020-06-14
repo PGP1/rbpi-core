ca75c47378584549d95a0080c797d1c555fcaafb
nugyn <linhduyn@gmail.com>
2020-06-14 17:02:32 +1000

    Added docstrings

--- functionality/register.py
+++ functionality/register.py
@@ -1,4 +1,6 @@
-#!/usr/bin/python2
+"""
+module that contains code that will register the raspberry pi device
+"""
 import json
 import utility
 import paho.mqtt.client as mqtt
@@ -14,14 +16,9 @@ BROKER_PORT = os.getenv("BROKER_PORT")
 
 
 def register():
-    '''
-    main methods
-    - connect()
-    - disconnect()
-    - subscribe()
-    - publish ()
-    '''
-
+    """
+    A class that contains register logic
+    """
     broker_address = BROKER_IP
     port = int(BROKER_PORT)
     print(port)
@@ -30,20 +27,65 @@ def register():
     payload = {'id': ID}
 
     def on_publish(client, userdata, result):
+        """
+        Callback function, activates implementation once the client.publish is successful
+
+        :param client: the client instance for this callback
+        :type client: Client
+        :param userdata: the private user data as set in Client() or
+        user_data_set()
+        :type userdata: any
+        :param result: Data being published
+        :type result: String
+        """
         print("registered device \n")
-        print(payload)
+        print(result)
         pass
-    
+
     def on_connect(client, userdata, flags, rc):
+        """
+        Callback function, activates implementation to run on connect
+
+        :param client: the mqtt client
+        :type client: Client
+        :param userdata: the private user data as set in Client()
+            or user_data_set()
+        :type userdata: [type]
+        :param rc: disconnection result
+        :type rc: int
+        """
         if rc == 0:
             print("connection established, returned code=", rc)
         else:
             print("connection error, returned code=", rc)
 
     def on_log(client, userdata, level, buf):
+        """
+        Callback function, activates implementation to run on log.
+
+        :param userdata: the private user data as set in Client() or
+        user_data_set()
+        :type userdata: any
+        :param level: severity of the message
+        :type level: MQTT_LOG_INFO, MQTT_LOG_NOTICE, MQTT_LOG_WARNING,
+        MQTT_LOG_ERR, MQTT_LOG_DEBUG
+        :param buf: message buffer
+        :type buf: bytes
+        """
         print("log ", buf)
 
     def on_disconnect(client, userdata, rc):
+        """
+        Callback function, activates implementation to run on disconnect
+
+        :param client: the mqtt client
+        :type client: Client
+        :param userdata: the private user data as set in Client()
+            or user_data_set()
+        :type userdata: [type]
+        :param rc: disconnection result
+        :type rc: int
+        """
         logging.debug("disconnected, rc=", str(rc))
         client.loop_stop()
 
