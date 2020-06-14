57347a70549cd2c9793a7afde111e812bf75f851
nugyn <linhduyn@gmail.com>
2020-06-14 11:04:11 +1000

    Combined test_mqtt, into test_sensor_collect()

--- test/test_sensor_collect.py
+++ test/test_sensor_collect.py
@@ -3,16 +3,17 @@ testing if mqtt protocol sends data properly
 """
 import serial
 import json
-
-# Established arduino connection
-ser = serial.Serial('/dev/ttyACM0', baudrate=9600, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS)
-s = [0]
-
 import paho.mqtt.client as mqtt
 import time
 import json
 
+# Established arduino connection
+ser = serial.Serial('/dev/ttyACM0', baudrate=9600, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS)
+s = [0]
 LOCALHOST = "localhost"
+
+# Init client
+client = mqtt.Client("Test")
 
 def test_sensor_collect():
     try:
@@ -21,15 +22,22 @@ def test_sensor_collect():
         payload = s[0]
         payloadJSON = json.loads(payload)
         print("[Serial] recieving data from Arduino | payload {}".format(payload))
-        test_mqtt(payloadJSON)
-        # publisher = Publisher()
-        # publisher.publish('arduino', payloadJSON['data'])
+        # Binds functions
+        topic = "test_topic"
+        client.on_connect = on_connect
+        client.on_message = on_message
+
+        client.connect(LOCALHOST) # Connect to broker
+        client.loop_start() # Start loop
+
+        client.subscribe(topic)
+        client.publish(topic, payloadJSON)
+
+        client.loop_stop() # Stop loop
+        client.disconnect() # disconnect
     except Exception as e: 
         print('[Error] Decoding JSON has failed')
         print('[Error]', e)
-
-# Init client
-client = mqtt.Client("Test")
 
 # Functions to bind subscriber client.
 def on_connect(client, userdata, flags, rc):
@@ -51,19 +59,4 @@ def on_message(client, userdata, msg):
         assert payload == HELLO_RESP
 
 def on_disconnect(client, userdata, flags, rc=0):
-    print("Disconnected")
-
-def test_mqtt():
-    # Binds functions
-    topic = "test_topic"
-    client.on_connect = on_connect
-    client.on_message = on_message
-
-    client.connect(LOCALHOST) # Connect to broker
-    client.loop_start() # Start loop
-
-    client.subscribe(topic)
-    client.publish(topic, payload)
-
-    client.loop_stop() # Stop loop
-    client.disconnect() # disconnect    
+    print("Disconnected")    
